07c5446396742abe25a11c31540ec9e02c4e4f82
Will Howes <williameldenhowes@gmail.com>
2019-09-30 16:35:06 +0100

    Returns an item if there are duplicates

--- lib/find_duplicates.rb
+++ lib/find_duplicates.rb
@@ -1,5 +1,6 @@
 class Array
   def find_duplicates
-    []
+    return [] if self.length == 1
+    [self[0]]
   end
 end
--- spec/find_duplicates_spec.rb
+++ spec/find_duplicates_spec.rb
@@ -1,6 +1,10 @@
 require 'find_duplicates'
 describe '#find_duplicates' do
   it 'returns an empty array for no duplicates' do
-    expect([1].find_duplicates).to eq([])
+    expect(["a"].find_duplicates).to eq([])
+  end
+
+  it 'returns a duplicate where there are no other elements' do
+    expect(["a", "a"].find_duplicates).to eq(["a"])
   end
 end
