1945616d2beafd43d0e71171df0dae5e6df7ea4c
eldenhowes <williameldenhowes@gmail.com>
2019-10-03 11:52:28 +0100

    New algorithm using a hash for all elements and setting to true if there are duplicates

--- lib/find_duplicates.rb
+++ lib/find_duplicates.rb
@@ -3,17 +3,31 @@
 
 class Array
 
+  # def find_duplicates
+  #   hash_of_elements = Hash.new
+  #   self.each do |word|
+  #     if self.count(word) > 1
+  #       hash_of_elements[word] = true
+  #     end
+  #   end
+  #   hash_of_elements.keys
+  # end
+
   def find_duplicates
-    self.to_enum.with_index.reverse_each do |word, index|
-      if self.count(word) < 2
-        self[index] = self.last
-        self.pop
+    hash_of_elements = {}
+    self.each do |x|
+
+      if hash_of_elements.key?(x)
+        hash_of_elements[x] = true
+      else
+        hash_of_elements[x] = false
       end
     end
-    self.uniq
+    duplicates = hash_of_elements.select{|key,value| value == true}
+    duplicates.keys
   end
 end
 
 
 # array_find_duplicates = Proc.new { |array| array.find_duplicates }
-# p CodeTimer.code_timer_for_arrays_given_size(array_find_duplicates, 40000)
+# p CodeTimer.code_timer_for_arrays_given_size(array_find_duplicates, 20000)
